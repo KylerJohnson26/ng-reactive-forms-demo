19006204f74d2eb8db9b10873d6a3f300e95bf6b
Kyler Johnson <kylerjohnson@Kylers-MacBook-Pro.local>
2018-05-02 20:00:21 -0400

    setting up signup form and basic validation through Validators from the ReactiveFormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +7,22 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
+  genders = ['male', 'female', 'Elect not to identify'];
 
   signupForm: FormGroup;
 
   ngOnInit()
   {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('Elect not to identify')
     });
+  }
+
+  onSubmit()
+  {
+    console.log(this.signupForm);
   }
 
 }
