625b0c295ee7a9e0e57cc65c51f51767e12cec10
Kyler Johnson <kylerjohnson@Kylers-MacBook-Pro.local>
2018-05-03 23:16:15 -0400

    Adding examples of FormGroup.setValue(), FormGroup.patchValue(), and FormGroup.reset() methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, EmailValidator, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -17,16 +18,39 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenUsernamesValidator.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),  
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmailValidator),  
       }),
       'gender': new FormControl('Elect not to identify'),
       'hobbies': new FormArray([])
+    });
+    // set values of form programmatically
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Kyler',
+        'email': 'k@k.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+    // set only specific values programatically
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'KJ'
+      }
     });
   }
 
   onSubmit()
   {
     console.log(this.signupForm);
+    this.signupForm.reset({
+      'userData': {
+        'username': '',
+        'email': ''
+      },
+      'gender': 'Elect not to identify',
+      'hobbies': []
+    })
   }
 
   onAddHobby()
@@ -38,10 +62,22 @@ export class AppComponent implements OnInit {
   forbiddenUsernamesValidator(control: FormControl) : { [s: string]: boolean }
   {
     if(this.forbiddenUsernames.indexOf(control.value) !== -1)
-    {
       return { 'nameIsForbidden': true };
-    }
     return null;
+  }
+
+  forbiddenEmailValidator(control: FormControl) : Promise<any> | Observable<any>
+  {
+    const promise = new Promise<any>( (resolve, reject) => {
+      // set timeout to simulate call to a server
+      setTimeout( () => {
+        if(control.value === 'test@test.com')
+          resolve({ 'emailIsForbidden': true });
+        resolve(null);
+      }, 1500);
+    });
+    
+    return promise;
   }
 
 }
